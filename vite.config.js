896222130a5f2202e3e95c268d34f4ca3966f53b
Richmano NASY <nasyrichmano@gmail.com>
2025-07-22 20:58:14 +0200

    fix: reset vite base for Vercel deployment

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/richmano-portfolio/',
   plugins: [react()],
   build: {
     outDir: 'dist',
